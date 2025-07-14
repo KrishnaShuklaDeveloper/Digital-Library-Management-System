a7580eb27bbfec6102dbbccddf3b541bb31b0172
Krishna Shukla <krishnashukla9219448687@gmail.com>
2025-07-14 18:09:11 +0530

    Update settings.py

--- Library_Management/settings.py
+++ Library_Management/settings.py
@@ -89,10 +89,10 @@ USE_I18N = True
 USE_TZ = True
 
 # Static Files
-STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+# STATIC_URL = '/staticfiles/'
+# STATICFILES_DIRS = [
+  #  os.path.join(BASE_DIR, "staticfiles"),
+# ]
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Media Files (for Book Images, etc.)
